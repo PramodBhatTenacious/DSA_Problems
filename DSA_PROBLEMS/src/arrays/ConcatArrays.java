5f18285b69cbc809db4c635169f4b4e183a07c69
Pramod Bhat <pramodbhat111555@gmail.com>
2024-01-25 11:05:21 +0530

    first commit-commiting all the files

---
+++ DSA_PROBLEMS/src/arrays/ConcatArrays.java
@@ -0,0 +1,50 @@
+package arrays;
+
+import java.util.*;
+
+public class ConcatArrays {
+
+	public static int[] getConcatenation(int[] nums) {
+
+		int n = nums.length;
+		int temp[] = new int[n * 2];
+
+		for (int i = 0; i < temp.length; i++) {
+
+			if (i > n || i == n) {
+
+				temp[i] = nums[i - n];
+			}
+			if (i < n)
+				temp[i] = nums[i];
+		}
+		/*
+		 * for(int i=0; i<temp.length; i++) { System.out.print("  "+ temp[i]);
+		 * System.out.println(" "); }
+		 */
+		return temp;
+	}
+
+	public static void main(String[] args) {
+		int len;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the length OR number of Inputs " + "that you would like to store");
+		len = sc.nextInt();
+		
+		
+		int nums[] = new int[len];
+		int res[] = new int[len*2];
+		
+		for (int i = 0; i < len; i++) {
+			nums[i] = sc.nextInt();
+		}
+
+		res=getConcatenation(nums);
+		for(int i=0; i<len*2; i++) {
+			System.out.println("  "+ res[i]);
+		}
+		sc.close();
+
+	}
+
+}
